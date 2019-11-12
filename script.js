1fe32cb6c7ab93062f9e5ff631dbab83e1e5417c
Paul Irish <paul.irish@gmail.com>
2019-11-12 15:23:16 -0800

    add util.js

--- script.js
+++ script.js
@@ -1,44 +1,7 @@
 import {QUANTILE_AT_VALUE, VALUE_AT_QUANTILE} from './math.js';
 import { fromEvent, combineLatest, interval } from 'rxjs';
 import { map, startWith, debounce } from 'rxjs/operators';
-
-// blingjs
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
-
-const NBSP = '\xa0';
-const numberFormatter = new Intl.NumberFormat();
-
-// thx Lighthouse's util.js
-function arithmeticMean(items) {
-  items = items.filter(item => item.weight > 0);
-  const results = items.reduce(
-    (result, item) => {
-      const score = item.score;
-      const weight = item.weight;
-      return {
-        weight: result.weight + weight,
-        sum: result.sum + score * weight,
-      };
-    },
-    {weight: 0, sum: 0}
-  );
-  return results.sum / results.weight || 0;
-}
-const RATINGS = {
-  PASS: {label: 'pass', minScore: 0.9},
-  AVERAGE: {label: 'average', minScore: 0.5},
-  FAIL: {label: 'fail'},
-};
-function calculateRating(score) {
-  let rating = RATINGS.FAIL.label;
-  if (score >= RATINGS.PASS.minScore) {
-    rating = RATINGS.PASS.label;
-  } else if (score >= RATINGS.AVERAGE.minScore) {
-    rating = RATINGS.AVERAGE.label;
-  }
-  return rating;
-}
+import {$, $$, calculateRating, arithmeticMean, NBSP, numberFormatter} from './util.js';
 
 /**
  * V6 weights
---
+++ util.js
@@ -0,0 +1,40 @@
+
+// blingjs
+export const $ = document.querySelector.bind(document);
+export const $$ = document.querySelectorAll.bind(document);
+
+export const NBSP = '\xa0';
+export const numberFormatter = new Intl.NumberFormat();
+
+// thx Lighthouse's util.js
+export function arithmeticMean(items) {
+  items = items.filter(item => item.weight > 0);
+  const results = items.reduce(
+    (result, item) => {
+      const score = item.score;
+      const weight = item.weight;
+      return {
+        weight: result.weight + weight,
+        sum: result.sum + score * weight,
+      };
+    },
+    {weight: 0, sum: 0}
+  );
+  return results.sum / results.weight || 0;
+}
+
+export function calculateRating(score) {
+	const RATINGS = {
+		PASS: {label: 'pass', minScore: 0.9},
+		AVERAGE: {label: 'average', minScore: 0.5},
+		FAIL: {label: 'fail'},
+	};
+
+  let rating = RATINGS.FAIL.label;
+  if (score >= RATINGS.PASS.minScore) {
+    rating = RATINGS.PASS.label;
+  } else if (score >= RATINGS.AVERAGE.minScore) {
+    rating = RATINGS.AVERAGE.label;
+  }
+  return rating;
+}
