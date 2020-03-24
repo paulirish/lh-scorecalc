59c74876b2d632fd8c6ab691b145120f3b2383c3
Glitch (how-much-faster) <none>
2020-03-24 11:54:26 -0700

    fix temp storage of v5 and v6

--- dist/calc.js
+++ dist/calc.js
@@ -1806,7 +1806,7 @@
 
       // Restore cached value if available, otherwise generate reasonable random stuff
       if (localStorage.metricValues) {
-        const cachedValues = JSON.parse(localStorage.metricValues);
+        const cachedValues = JSON.parse(localStorage[`metricValues.${container.id}`]);
         elem.value = cachedValues[metricId];
       } else {
         elem.value = Math.max((Math.random() * (max - min)) / 2, min);
@@ -1856,7 +1856,7 @@
       )
       .pipe(debounce(() => interval(500)))
       .subscribe(values => {
-        localStorage.metricValues = JSON.stringify(values);
+        localStorage[`metricValues.${container.id}`] = JSON.stringify(values);
       });
 
     // Setup the score sliders
