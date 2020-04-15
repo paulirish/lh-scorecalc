9e0c5601f4596985724ecf8e199fa71adee7548e
Paul Irish <commits@paul.irish>
2020-04-15 11:49:15 -0700

    split setPerfGauge

--- script/gauge.js
+++ script/gauge.js
@@ -8,10 +8,14 @@ export function updateGauge(container, score) {
   wrapper.className = 'lh-gauge__wrapper'; // clear any other labels already set
   wrapper.classList.add(`lh-gauge__wrapper--${calculateRating(score / 100)}`);
 
-  const gaugeArc = container.$('.lh-gauge-arc');
+  _setPerfGauge(container, score);
+}
+
+function _setPerfGauge(wrapper, score) {
+  const gaugeArc = wrapper.$('.lh-gauge-arc');
   gaugeArc.style.strokeDasharray = `${(score / 100) * 352} 352`;
 
   const scoreOutOf100 = Math.round(score);
-  const percentageEl = container.$('.lh-gauge__percentage');
+  const percentageEl = wrapper.$('.lh-gauge__percentage');
   percentageEl.textContent = scoreOutOf100.toString();
 }
