f12c50c6bb88ab33b324e27634abdbf563f1a45d
Paul Irish <paul.irish@gmail.com>
2020-05-07 14:00:45 -0700

    Update script/main.js
    Co-authored-by: Connor Clark <cjamcl@gmail.com>

--- script/main.js
+++ script/main.js
@@ -194,7 +194,7 @@ const debounce = (callback, time = 250, interval) =>
   ((...args) => {
     clearTimeout(interval);
     interval = setTimeout(() => callback(...args), time);
-  })
+  });
 
 class App extends Component {
   constructor(props) {
