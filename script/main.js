27d7a0225e554f6e50e3a505f6ec35e317a9d8cf
Paul Irish <paul.irish@gmail.com>
2020-06-04 09:20:18 -0700

    Update script/main.js
    Co-authored-by: Connor Clark <cjamcl@gmail.com>

--- script/main.js
+++ script/main.js
@@ -235,7 +235,7 @@ class App extends Component {
     let device = params.get('device');
     // Default to mobile if it's not matching our known emulatedFormFactors. https://github.com/GoogleChrome/lighthouse/blob/master/types/externs.d.ts#:~:text=emulatedFormFactor
     if (device && device !== 'mobile' && device !== 'desktop') {
-      console.log('Invalid emulatedFormFactors value:', device, '. Fallback to mobile scoring.')
+      console.warning(`Invalid emulatedFormFactors value: ${device}. Fallback to mobile scoring.`);
       device = 'mobile';
     } else if (!device) {
       // Device not expressed in the params
